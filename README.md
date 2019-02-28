4fe398ed9ce7e4c0e5f35c04d67b9b4dd4d93d65
dxvo <dxvo@uh.edu>
2019-02-27 21:22:54 -0600

    Final

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mission to Mars 
-![mission_to_mars](Images/mission_to_mars.jpg)
+![Mission to Mars Page](Images/mission_to_mars.jpg)
 
 In this assignment, you will build a web application that scrapes various websites for data related to the Mission to Mars and displays the information in a single HTML page. The following outlines what you need to do.
 
--- app.py
+++ app.py
@@ -1,23 +1,29 @@
 from flask import Flask, render_template, redirect
-import pymongo
+from flask_pymongo import PyMongo
 import scrape_mars as sc  
 
-
-
-# Create an instance of our Flask app.
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-	mars = sc.scrape()
-	#mars = sc.scrape()
-	return render_template("index.html", mars = mars)
 
-@app.route("/scrape")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/surfing_app")
+mars = sc.scrape()
+
+@app.route('/')
+def index():
+    #mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
+
+
+@app.route('/scrape')
 def scrape():
-	new_data = sc.scrape()
-	return render_template("index.html", mars = new_data)
-
+    mars = mongo.db.mars
+    data = sc.scrape()
+    mars.update(
+        {},
+        data,
+        upsert=True
+    )
+    return redirect("/", code=302)
 
 if __name__ == "__main__":
     app.run(debug=True)
